ad72a9f4c3b2bc690eaf85b3cb49d95e4c1a9be7
Saurabh Rai <raisaurabh625@gmail.com>
2025-01-18 15:04:54 +0530

    succ

---
+++ if_else.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class if_else
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a==1)
+            System.out.println("one");
+        else if(a==2)
+            System.out.println("two");
+        else if(a==3)
+            System.out.println("three");
+        else if(a==4)
+            System.out.println("four");
+        else if(a==5)
+            System.out.println("five");
+        else if(a==6)
+            System.out.println("six");
+        else if(a==7)
+            System.out.println("seven");
+        else if (a==8)
+            System.out.println("eight");
+        else if(a==9)
+            System.out.println("nine");
+        else
+            System.out.println("NOT A VALID NUMBER");
+    }
+}
